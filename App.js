4362bc83fa4d6353902542bb28b89c927b65099e
okimotolen <ollxxx0912@gmail.com>
2020-06-25 21:13:20 +0900

    settings for eslint

--- App.js
+++ App.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
+export default class App extends React.Component {
   render() {
-   return (
+    return (
       <View style={styles.container}>
         <Text>Hello!</Text>
       </View>
